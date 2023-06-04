bb645e9b2d98cf691c753ce9e428de4f00d1b8b7
lt-link <135496928+lt-link@users.noreply.github.com>
2023-06-04 14:29:14 +0800

    Delete ahd directory

--- ahd/play/play.sh
+++
@@ -1,4 +0,0 @@
-xawtv -c /dev/video0 &
-xawtv -c /dev/video1 &
-xawtv -c /dev/video2 &
-xawtv -c /dev/video3 &
--- ahd/play/play2.sh
+++
@@ -1,1 +0,0 @@
-gst-launch-1.0 v4l2src device=/dev/video0 ! video/x-raw,format=YUY2,width=1280,height=720,framerate=60/1 ! queue2 ! videorate ! videoscale ! videoconvert ! xvimagesink
